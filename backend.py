20f6a2462ccba7154668936018ffad8c33f8a2f5
Hassan-Sanaullah <136129863+Hassan-Sanaullah@users.noreply.github.com>
2023-06-10 23:27:42 +0500

    Add files via upload
    Changed file names

--- backend.py
+++ backend.py
@@ -1,16 +1,4 @@
-# from client import socketCall
-# import tkinter as tk
-
-
-# def new(username, passwd):
-#     text = 'Account'
-#     socketCall(str(username), True)
-#     socketCall(str(passwd), True)
-#     socketCall('exit', False)
-    
-#     print(username, passwd)
-
-from gui import Connect, Disconnect, communicate
+from client import Connect, Disconnect, communicate
 import tkinter as tk
 
 def estConnection():
--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import socket
 
-def socketCall(message, loop):
-    # Server IP and port
+#Creates socket and establishes connection
+def Connect():
     server_ip = "127.0.0.1"
     server_port = 8080
 
     # Create a TCP client socket
+    global client_socket 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
@@ -13,28 +14,27 @@ def socketCall(message, loop):
         client_socket.connect((server_ip, server_port))
         print("Connected to the server.")
 
-        
-        if loop == True:
-            # Send data to the server
-            #message = input("Enter a message to send: ")
-            client_socket.sendall(message.encode())
-
-            # Receive data from the server
-            data = client_socket.recv(1024).decode()
-            print("Received from server:", data)
-
-            
-            # if message == "exit":
-            #     break
-
     except ConnectionRefusedError:
-        print("Connection refused. Make sure the server is running.")
+            print("Connection refused. Make sure the server is running.")
     except Exception as e:
         print("An error occurred:", str(e))
-    finally:
-        if loop == False:
-            # Close the socket
-            client_socket.close()
 
-# if __name__ == '__main__':
-#     main()
+# Sending message to server
+def communicate(message):
+
+    # Send data to the server
+    #message = input("Enter a message to send: ")
+    client_socket.sendall(message.encode())
+
+    # Receive data from the server
+    data = client_socket.recv(1024).decode()
+    print("Received from server:", data)
+
+                    
+    # if message == "exit":
+    #     break
+
+#Disconnects from server
+def Disconnect():       
+    # Close the socket
+    client_socket.close()
